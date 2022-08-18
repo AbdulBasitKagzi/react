884faed21f39dc97142949a3119b2058b3ec9179
AbdulBasitKagzi <100257953+AbdulBasitKagzi@users.noreply.github.com>
2022-08-18 16:49:22 +0530

    Delete expense-app directory

--- expense-app/src/components/UI/Card.js
+++
@@ -1,9 +0,0 @@
-import "./Card.css";
-
-const Card = (props) => {
-  // the below variable is to specify to our card component to also use another classes for css
-  const classes = "card " + props.className;
-  return <div className={classes}>{props.children}</div>;
-};
-
-export default Card;
