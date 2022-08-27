8230bcb1bfdf087d66f025823937c22f24cbca31
abdulbasit <imabk@gmail.com>
2022-08-27 15:48:36 +0530

    Completed

--- MovieList/src/App.js
+++ MovieList/src/App.js
@@ -1,31 +1,83 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
 
+  // const fetchMoviesHandler = () => {
+  //   fetch("https://swapi.dev/api/films")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       const transformedData = data.results.map((movieList) => {
+  //         return {
+  //           id: movieList.episode_id,
+  //           title: movieList.title,
+  //           release: movieList.release_date,
+  //           openingText: movieList.opening_crawl,
+  //         };
+  //       });
+  //       setMovies(transformedData);
+  //     });
+  // };
+
+  const [moviesState, setMoviesState] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    if (moviesState === false) return;
+
+    try {
+      setIsLoading(true);
+      fetch("https://swapi.dev/api/film")
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Error hai bhai ****");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          const transformedData = data.results.map((movieList) => {
+            return {
+              id: movieList.episode_id,
+              title: movieList.title,
+              release: movieList.release_date,
+              openingText: movieList.opening_crawl,
+            };
+          });
+          setMovies(transformedData);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.log(error.message);
+          setError(error.message);
+          setIsLoading(false);
+        });
+    } catch (error) {
+      console.log("ADFsdf", error);
+      setError(error.message);
+      setIsLoading(false);
+    }
+  }, [moviesState]);
+
+  const setMoviesStateHandler = () => {
+    setMoviesState(true);
+    setTimeout(() => {
+      setMoviesState(false);
+    }, 300);
+  };
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={setMoviesStateHandler}> Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {!isLoading && error && <p>{error}</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
