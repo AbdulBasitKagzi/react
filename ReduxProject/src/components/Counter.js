fe35caa287f7b2b9e73d08dae697c3aec2e81f77
abdulbasit <imabk@gmail.com>
2022-09-02 18:08:26 +0530

    Remaining--Adding redux to login/signup page

--- ReduxProject/src/components/Counter.js
+++ ReduxProject/src/components/Counter.js
@@ -1,13 +1,51 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
+import { counterActions } from "../store/index";
+// custom hook provided by redux
+import { useSelector, useDispatch } from "react-redux/es/exports";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const counter = useSelector((state) => state.counter.counter);
+  const showCounter = useSelector((state) => state.counter.showCounter);
+  // useDispatch will give us function which is stored in dispatch and  dispatch can be used to disptach action
 
+  const dispatch = useDispatch();
+
+  // increment function to handle increment
+  const incrementHandler = () => {
+    // dispatch will dispatch an action which will be used in redux store to handle increment
+    // counterActions.increment() will provide us action object like this : ({type:"some auto generated unique indentifier"})
+    // eg: ({type:"increment"})
+    dispatch(counterActions.increment());
+  };
+
+  // decrement function to handle deccrement
+  const decrementHandler = () => {
+    // dispatch will dispatch an action which will be used in redux store to handle increment
+    // counterActions.increment() will provide us action object like this : ({type:"some auto generated unique indentifier"})
+    // eg: ({type:"decrement"})
+    dispatch(counterActions.decrement());
+  };
+
+  const increaseHandler = () => {
+    // here the amount which will be dispatched by the dispatcher is known as payload
+    dispatch(counterActions.increase(5));
+  };
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggle());
+  };
+  const increasebysamevalueHandler = () => {
+    dispatch(counterActions.increasebysamevalue());
+  };
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
+
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <button onClick={incrementHandler}>Incremet Counter</button>
+      <button onClick={decrementHandler}>Decremet Counter</button>
+      <button onClick={increaseHandler}>IncreaseBy 5</button>
+      <button onClick={increasebysamevalueHandler}>IncreaseBysameValue</button>
     </main>
   );
 };
---
+++ ReduxProject/src/store/index.js
@@ -0,0 +1,78 @@
+// import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+const initialCounterState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialCounterState,
+  reducers: {
+    increment: (state) => {
+      state.counter++;
+    },
+    decrement: (state) => {
+      state.counter--;
+    },
+    increase: (state, action) => {
+      state.counter = state.counter + action.payload;
+    },
+    toggle: (state) => {
+      state.showCounter = !state.showCounter;
+      state.counter = state.counter - state.counter;
+    },
+    increasebysamevalue: (state) => {
+      state.counter = state.counter + state.counter;
+    },
+  },
+});
+
+// reducer function
+// const countReducer = (state = initialState, action) => {
+//   if (action.type === "increment") {
+//     return {
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   if (action.type === "decrement") {
+//     return {
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   if (action.type === "increase") {
+//     return {
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   if (action.type === "toggle") {
+//     state.counter = 0;
+//     return {
+//       showCounter: !state.showCounter,
+//       counter: state.counter,
+//     };
+//   }
+//   if (action.type === "increasebysamevalue") {
+//     return {
+//       counter: state.counter + state.counter,
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   return state;
+// };
+
+// store
+
+// const reducer = {
+//   counter: createSlice.reducers,
+// };
+
+const store = configureStore({
+  reducer: { counter: counterSlice.reducer },
+});
+
+// actions will provide us action object like this : ({type:"some auto generated unique indentifier"})
+// eg: ({type:"increment"})
+export const counterActions = counterSlice.actions;
+export default store;
